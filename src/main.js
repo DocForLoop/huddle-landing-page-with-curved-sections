2e93eb045ba75aa411f0cbdbce391ce54b7fa164
Csaba Molnar <docforloop@proton.me>
2025-03-26 17:31:43 +0100

    Add desktop styles

--- src/main.js
+++ src/main.js
@@ -6,19 +6,19 @@ const emailError = document.getElementById('email-error');
 const modal = document.querySelector('.modal');
 const modalCloseButton = document.querySelector('.modal__btn');
 const modalEmail = document.querySelector('.modal__email');
+const submitButton =  document.querySelector('.newsletter__btn');
 const emailPattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
 
 
 const handleSubmit = e => {
     e.preventDefault();
 
-    const submitButton =  document.querySelector('.newsletter__btn');
     const formData = new FormData(e.target);
     const {email} = Object.fromEntries(formData);
     const cleanEmail = email.trim().toLowerCase();
     
     if (cleanEmail === '' || !emailPattern.test(cleanEmail)) {
-        emailError.textContent = 'Check your email please';
+        emailError.textContent = 'Valid email required';
         emailInput.classList.add('newsletter__email-input--error');
         return;
     }
@@ -35,14 +35,19 @@ const handleSubmit = e => {
     }, 1500);
 }
 
+const removeError = () => {
+    emailError.textContent = '';
+    emailInput.classList.remove('newsletter__email-input--error');
+}
+
 const checkInput = () => {
     const emailValue = emailInput.value.trim().toLowerCase();
 
     if (emailValue !== '' && emailPattern.test(emailValue)) {
-        emailError.textContent = '';
-        emailInput.classList.remove('newsletter__email-input--error');
+        removeError();
     }
 }
+
 
 const closeModal = () => {
     modal.close();
@@ -57,12 +62,13 @@ const closeModal = () => {
 }
 
 const handleEscape = e => {
-    if (e.key === 'Escape') {
+    if (e.key === 'Escape' && modal.open) {
         closeModal();
     }
 }
 
 form.addEventListener('submit', handleSubmit);
 emailInput.addEventListener('input', checkInput);
+emailInput.addEventListener('blur', removeError);
 modalCloseButton.addEventListener('click', closeModal);
 document.addEventListener('keydown', handleEscape);
